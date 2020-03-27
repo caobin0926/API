6d20e8d7892cfbdbe0e70ebc194ff977613f7c2d
caobin@tuandai.com <caobin@tuandai.com>
2020-03-27 21:41:59 +0800

    修改注册接口测试用例

--- API_1/testcases/test_register.py
+++ API_1/testcases/test_register.py
@@ -8,35 +8,44 @@ from API_1.common.do_re import replace
 import json
 class TestRegister:
     """测试注册接口类"""
-    ex=DoExcel(path.case_file,'register')
-    data=ex.get_cass()
+    ex = DoExcel(path.case_file, 'register')
+    case = ex.get_cass()
+
     def setup_class(self):
-        self.request=HttpRequests()
-        self.logs=Logs('register')
-        self.logs.loggers('INFO','----------开始执行测试用例---------')
-    @allure.feature('注册接口')
-    @allure.suite('注册接口测试用例')
-    @pytest.mark.parametrize('case',data)
-    def test_register(self,case):
-        allure.attach('请求参数：{}'.format(case.data))
-        self.logs.loggers('INFO', '请求方式:{};请求地址:{};请求参数:{}'.format(case.method, case.url, case.data))
-        case.data=replace(case.data)
-        resp=self.request.requests(case.method,case.url,data=eval(case.data))
-        case.actual=json.loads(resp.text)
-        allure.attach('期望结果:{}'.format(case.expected))
-        allure.attach('实际结果:{}'.format(case.actual))
-        self.logs.loggers('INFO','响应参数：{}'.format(case.actual))
+        self.http = HttpRequests()
+        self.ex = DoExcel(path.case_file, 'register')
+        self.loger = Logs('logger')
+        self.loger.loggers('INFO', '-------------开始执行测试用例------------')
+
+    @allure.story('测试场景')
+    @allure.suite('登录测试用例')
+    @pytest.mark.parametrize('cases', case)
+    def test_login(self, cases):
+        # global loger
+        allure.attach('请求参数：{}'.format(cases.data))
+        self.loger.loggers('INFO', '请求方式:{};请求地址:{};请求参数:{}'.format(cases.method, cases.url, cases.data))
+        cases.data=replace(cases.data)
+        resp = self.http.requests(cases.method, url=cases.url, data=eval(cases.data))
+        allure.attach('期望结果:{}'.format(cases.expected))
+        allure.attach('响应结果:{}'.format(resp.text))
+        cases.actual = json.loads(resp.text)
+        self.loger.loggers('INFO', '响应报文:{}'.format(cases.actual))
         try:
-            assert eval(case.expected)==case.actual
-            case.result='PASS'
+            assert (eval(cases.expected) == cases.actual)
+            cases.result = 'PASS'
         except AssertionError as e:
-            case.result='FAIL'
-            self.logs.loggers('error', '错误原因:{}'.format(e))
+            cases.result = 'FAIL'
+            self.loger.loggers('ERROR', '报错原因:{}'.format(e))
             raise e
         finally:
-            allure.attach('测试结果：{}'.format(case.result))
-            self.logs.loggers('INFO','断言结果:{}'.format(case.result))
-            TestRegister.ex.write_excel(int(case.case_id)+1,str(case.actual),case.result)
+            allure.attach('测试结果:{}'.format(cases.result))
+            # ex = DoExcel(path.case_file, 'login')
+            self.ex.write_excel(int(cases.case_id) + 1, str(cases.actual), cases.result)
+            self.loger.loggers('INFO', '测试结果:{}'.format(cases.result))
+
+    def teardown_class(self):
+        self.http.seesion.close()
+        self.loger.loggers('INFO', '------------登录测试用例全部完成----------')
 
 
 
